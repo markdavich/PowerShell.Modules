b2a7345627b95851c6afa3bdbe5d80dec0f78ebe
Mark Davich <Mark.Davich@its.idaho.gov>
2025-07-23 23:00:44 -0600

    Finished tracker architecture

--- Modules/Cs.Type.Tracker/.cs/Tracker.cs
+++ Modules/Cs.Type.Tracker/.cs/Tracker.cs
@@ -3,7 +3,9 @@ using System.Collections.Generic;
 
 namespace Cs.Type;
 
-
+/// <summary>
+/// Represents the direction of an item movement within a list.
+/// </summary>
 public enum ListDirection
 {
     None,
@@ -11,11 +13,19 @@ public enum ListDirection
     Down
 }
 
+/// <summary>
+/// Contains metadata about an item that was moved in a list, including
+/// original index, new index, and the direction of movement.
+/// </summary>
 public class ItemMoveInfo
 {
     public ListDirection Direction { get; internal set; } = ListDirection.None;
     public int? Index { get; internal set; } = null;
     public int? OriginalIndex { get; internal set; } = null;
+
+    /// <summary>
+    /// Indicates whether the item was actually moved.
+    /// </summary>
     public bool WasMoved =>
         OriginalIndex.HasValue
         &&
@@ -24,183 +34,166 @@ public class ItemMoveInfo
         (OriginalIndex.Value != Index.Value);
 }
 
+/// <summary>
+/// Internal strategy class implementing the Strategy pattern to encapsulate
+/// behavior for moving items in different directions within an OrderedSet.
+/// </summary>
+internal class MoveStrategy<TItem>
+{
+    private readonly OrderedSet<TItem> Set;
+    private readonly Dictionary<ListDirection, Action<ItemMoveInfo>> Strategy;
+
+    public MoveStrategy(OrderedSet<TItem> orderedSet)
+    {
+        Set = orderedSet;
+        Strategy = new()
+        {
+            [ListDirection.Up] = i =>
+            {
+                if (Set.Set.TryGetValue(Set.Items[i.OriginalIndex!.Value], out TItem item))
+                {
+                    Set.Items.RemoveAt(i.OriginalIndex.Value);
+                    Set.Items.Insert(i.Index!.Value, item);
+                }
+            },
+            [ListDirection.Down] = i =>
+            {
+                if (Set.Set.TryGetValue(Set.Items[i.OriginalIndex!.Value], out TItem item))
+                {
+                    Set.Items.Insert(i.Index!.Value, item);
+                    Set.Items.RemoveAt(i.OriginalIndex.Value);
+                }
+            },
+            [ListDirection.None] = _ => { }
+        };
+    }
+
+    /// <summary>
+    /// Executes the appropriate move operation based on the direction.
+    /// </summary>
+    public void Process(ItemMoveInfo info)
+    {
+        Strategy[info.Direction](info);
+    }
+}
+
+/// <summary>
+/// An ordered set implementation that preserves insertion order and supports
+/// controlled item movement (e.g., up, down, top, end) while ensuring uniqueness.
+/// </summary>
 public class OrderedSet<TItem>
 {
-    private HashSet<TItem> Set { get; } = new();
+    /// <summary>
+    /// Internal backing set used to guarantee uniqueness.
+    /// </summary>
+    internal HashSet<TItem> Set { get; } = new();
+
+    /// <summary>
+    /// The ordered list of items.
+    /// </summary>
     public List<TItem> Items { get; } = new();
 
-    public OrderedSet OrderedSet(TItem[] items)
+    public OrderedSet(params TItem[] items)
     {
-        for (int i = 0; i < items.Length; i++)
+        foreach (var item in items)
         {
-            TItem item = items[i];
-            bool added = Set.Add(item);
-            if (added)
-            {
-                TItem reference;
-                bool found = Set.TryGetValue(item, out add);
+            _ = Add(item);
+        }
+    }
 
-                if (found)
-                {
-                    Items.Add(reference);
-                    ListOrder[reference] = ListOrder.Count;
-                }
+    /// <summary>
+    /// Removes an item from the set while preserving order.
+    /// </summary>
+    public TItem Remove(TItem item)
+    {
+        if (Set.TryGetValue(item, out TItem reference))
+        {
+            Items.Remove(reference);
+            Set.Remove(reference);
+            return reference;
+        }
+
+        return item;
+    }
+
+    /// <summary>
+    /// Adds a unique item to the end of the list.
+    /// </summary>
+    public TItem Add(TItem item)
+    {
+        if (Set.Add(item))
+        {
+            if (Set.TryGetValue(item, out TItem reference))
+            {
+                Items.Add(reference);
+                return reference;
             }
         }
+
+        return item;
     }
 
     public ItemMoveInfo MoveDown(TItem item) => MoveItem(item, 1);
-
     public ItemMoveInfo MoveUp(TItem item) => MoveItem(item, -1);
-
     public ItemMoveInfo MoveToEnd(TItem item) => MoveItem(item, Items.Count);
-
     public ItemMoveInfo MoveToTop(TItem item) => MoveItem(item, -Items.Count);
 
+    /// <summary>
+    /// Orchestrates item movement logic: indexing, direction, and applying the move.
+    /// </summary>
     private ItemMoveInfo MoveItem(TItem item, int increment)
     {
-
         ItemMoveInfo result = new();
-
-        if (Set.TryGetValue(item, out TItem reference))
-        {
-            ProcessNextIndex(item, ref result);
-        }
-
+        ProcessIndex(result, item, increment);
+        ProcessDirection(result);
         ProcessMove(result);
-
         return result;
     }
 
-    private void ProcessNextIndex(TItem item, int increment, ref ItemMoveInfo info)
+    /// <summary>
+    /// Calculates and clamps the new index based on the increment.
+    /// Sets OriginalIndex and Index on the info object.
+    /// This method handles only index logic and allows separation of responsibilities.
+    /// </summary>
+    private void ProcessIndex(ItemMoveInfo info, TItem item, int increment)
     {
         int currentIndex = Items.IndexOf(item);
 
         if (currentIndex < 0)
-        {
             return;
-        }
 
         info.OriginalIndex = currentIndex;
-
-        ProcessIndex(ref info, increment);
-        ProcessDirection(ref info);
+        int newIndex = currentIndex + increment;
+        info.Index = Math.Clamp(newIndex, 0, Items.Count - 1);
     }
 
-    private int ProcessIndex(ref ItemMoveInfo info, int increment)
-    {
-        int newIndex = info.OriginalIndex + increment;
-
-        if (newIndex >= Items.Count)
-        {
-            info.Index = Items.Count - 1;
-            return;
-        }
-
-        if (newIndex < 0)
-        {
-            info.Index = 0;
-            return;
-        }
-    }
-
-    private ListDirection ProcessDirection(ref ItemMoveInfo info)
+    /// <summary>
+    /// Determines movement direction (Up, Down, None) from index comparison.
+    /// Keeps direction logic isolated to allow reuse and testing.
+    /// </summary>
+    private void ProcessDirection(ItemMoveInfo info)
     {
         if (info.Index > info.OriginalIndex)
         {
             info.Direction = ListDirection.Down;
-            return;
         }
-
-        if (info.Index < info.OriginalIndex)
+        else if (info.Index < info.OriginalIndex)
         {
             info.Direction = ListDirection.Up;
-            return;
         }
-
-        info.Direction = ListDirection.None;
-    }
-
-    private void ProcessMove(ref ItemMoveInfo info)
-    {
-        switch (info.Direction)
+        else
         {
-            case ListDirection.Up:
-                ProcessMoveUp(info);
-                break;
-
-            case ListDirection.Down:
-                ProcessMoveDown(info);
-                break;
+            info.Direction = ListDirection.None;
         }
-    }
-
-    private void ProcessMoveUp(ItemMoveInfo info)
-    {
-        // When moving up we can remove the item without affecting the
-        // index where the item will be inserted. So we just remove the
-        // item at the original index and insert a reference at the new
-        // index.
-        if (Set.TryGetValue(Items[info.OriginalIndex], out TItem referenceItem))
-        {
-            Items.RemoveAt(info.OriginalIndex);
-            Items.Insert(info.Index, referenceItem);
-        }
-    }
-
-    private void ProcessMoveDown(ItemMoveInfo info)
-    {
-        // When moving an item down, we can insert a reference a the index,
-        // which will not affect the current index of the item, after the
-        // insert we remove the item at the original index.
-        if (Set.TryGetValue(Items[info.OriginalIndex], out TItem referenceItem))
-        {
-            Items.Insert(info.Index, referenceItem);
-            Items.RemoveAt(info.OriginalIndex);
-        }
-    }
-}
-
-public class Tracker<TItem>(TItem[] toDos)
-{
-    private int _initialCount = 0;
-
-    public HashSet<TItem> SubList { get; private set; } = new(toDos);
-
-    public HashSet<TItem> ToDo { get; private set; } = new(toDos);
-
-    public HashSet<TItem> Done { get; private set; } = [];
-
-    /// <summary>
-    /// Completes the Item by removing it from ToDo and adding it to Done
-    /// </summary>
-    /// <param name="item"></param>
-    public void Complete(TItem item)
-    {
-        _ = ToDo.Remove(item);
-        _ = Done.Add(item);
     }
 
     /// <summary>
-    /// Items is a sub-list of objects that you have in "ToDo", when you invoke<br></br>
-    /// <b>Start</b>, Tracker starts tracking the sub-list, "items". You can track<br></br>
-    /// your progress by checking <see cref="IsFinished()"/> and <see cref="GetIncomplete()"/>  
+    /// Delegates the actual item movement to a strategy implementation.
+    /// This step is the final action in the micro-state machine for MoveItem.
     /// </summary>
-    /// <param name="items"></param>
-    public void Start(TItem[] items)
+    private void ProcessMove(ItemMoveInfo info)
     {
-        SubList = new(items);
-        _initialCount = Done.Count;
+        var strategy = new MoveStrategy<TItem>(this);
+        strategy.Process(info);
     }
-
-    public void Finish()
-    {
-        SubList = new(ToDo);
-        _initialCount = ToDo.Count;
-    }
-
-    public bool IsFinished => (Done.Count - _initialCount) == SubList.Count;
-
-    public TItem[] Incomplete =>
-        SubList.Where(item => !Done.Contains(item)).ToArray();
 }
