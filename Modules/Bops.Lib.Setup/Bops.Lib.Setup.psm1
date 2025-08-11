361d972352d5f61cbf199d7fc8d0dbc76e3b5ac7
Mark Davich <Mark.Davich@its.idaho.gov>
2025-08-11 14:34:00 -0600

    Use .json and constants in Bops.Lib.Setup

--- Modules/Bops.Lib.Setup/Bops.Lib.Setup.psm1
+++ Modules/Bops.Lib.Setup/Bops.Lib.Setup.psm1
@@ -5,6 +5,7 @@ using module Bop.U.Parser.Json
 using module Bop.U.Variable
 using module Bop.U.Variable.Logger
 using module Bop.U.FileSystem
+
 using module '.\Bops.Lib.Setup.Config.psm1'
 
 Write-Host "<[" -ForegroundColor Green -NoNewline
@@ -17,37 +18,18 @@ Write-Host "]" -ForegroundColor Green
 # │ Configuration │
 # ╰───────────────╯
 # Constants
+$configuration = @(
+    (Add-Local 'ExplorerRegistryKey' 'HKCU:\SOFTWARE\Classes\CLSID\{52205fd8-5dfb-447d-801a-d0b52f2e83e1}'),
+    (Add-Local 'ExplorerCommandRegistryKey' "$ExplorerRegistryKey\shell\opennewwindow\command"),
+    (Add-Local 'ModulePath' $MyInvocation.MyCommand.Path),
+    (Add-Local 'SettingsJsonFile' (Get-CompanionName -FileString $ModulePath -CompanionExtension 'json')),
+    (Add-Local 'ConfigJsonFile' (Get-CompanionName -FileString $ModulePath -CompanionExtension 'Config.json')),
+    (Add-Local 'Config' ([JsonParser]::new($ConfigJsonFile, [Config]).GetInstance())),
+    (Add-Local 'VbsPath' (Join-Path ([System.IO.Path]::GetDirectoryName($ModulePath)) $Config.VbsFileName)),
+    (Add-Local 'VbsDefaultContent' (Join-Path ([System.IO.Path]::GetDirectoryName($ModulePath)) $Config.VbsDefaultContent))
+)
 
-$V1 = [Variable]::new('VOne', 'V-1', $ExecutionContext.SessionState)
-$V2 = [Variable]::new('VTwo', "$VOne-V-2", $ExecutionContext.SessionState)
-
-$VarExplorerRegistryKey = (Add-Local 'ExplorerRegistryKey' 'HKCU:\SOFTWARE\Classes\CLSID\{52205fd8-5dfb-447d-801a-d0b52f2e83e1}')
-$VarExplorerCommandRegistryKey = (Add-Local 'ExplorerCommandRegistryKey' "$ExplorerRegistryKey\shell\opennewwindow\command")
-$VarModulePath = (Add-Local 'ModulePath' $MyInvocation.MyCommand.Path)
-$VarJsonConfigFile = (Add-Local 'JsonConfigFile' (Get-CompanionName -FileString $ModulePath -CompanionExtension 'Config.json'))
-$VarConfig = (Add-Local 'Config' [JsonParser]::new($JsonConfigFile, [Config]))
-$VarJsonPath = (Add-Local 'JsonPath' (Get-CompanionName -FileString $ModulePath -CompanionExtension 'json'))
-$VarVbsPath = (Add-Local 'VbsPath' (Join-Path [System.IO.Path]::GetDirectoryName($ModulePath) $Config.vbsFileName))
-
-
-# $configuration = @(
-#     (AddVariable 'ExplorerRegistryKey', 'HKCU:\SOFTWARE\Classes\CLSID\{52205fd8-5dfb-447d-801a-d0b52f2e83e1}'),
-#     (AddVariable 'ExplorerCommandRegistryKey', "$ExplorerRegistryKey\shell\opennewwindow\command"),
-#     (AddVariable 'ModulePath', $MyInvocation.MyCommand.Path),
-#     (AddVariable 'JsonConfigFile', (Get-CompanionName -FileString $ModulePath -CompanionExtension 'Config.json')),
-#     (AddVariable 'Config', [JsonParser]::new($JsonConfigFile, [Config])),
-#     (AddVariable 'JsonPath', (Get-CompanionName -FileString $ModulePath -CompanionExtension 'json')),
-#     (AddVariable 'VbsPath', (Join-Path [System.IO.Path]::GetDirectoryName($ModulePath) $Config.vbsFileName)),
-# )
-
-[VariableLogger]::Print("Bops.Lib.Setup Configuration Variables", $configuration, [System.ConsoleColor]::Yellow)
-
-# $modulePath = Split-Path -Parent $MyInvocation.MyCommand.Path
-# $jsonPath = Join-Path $modulePath 'Bops.Lib.Setup.json'
-# $vbsPath = Join-Path $modulePath 'launch.location.vbs'
-
-
-# Import-Module -Path "$PSScriptRoot\Bops.Lib.Setup.Settings.psm1"
+[VariableLogger]::Print("Bops.Lib.Setup Configuration Variables", $configuration, [System.ConsoleColor]::Cyan)
 
 function Get-ExplorerLocation {
     $settings = Get-UserSettings
@@ -59,10 +41,23 @@ function Set-ExplorerLocation {
     param (
         [string] $Location
     )
-    throw [System.NotImplementedException] "Set-ExplorerLocation not fully implemented"
+    
+    if (-not (Test-Path $Location)) {
+        Write-Host
+        Write-Host "!!! Location Does Not Exist" -ForegroundColor Red
+        Write-Host "-->  $Location" -ForegroundColor Magenta
+        Write-Host
+        Write-Host "Please pick a different location"
+        Write-Host
+
+        return
+    }
+
+
     $settings = Get-UserSettings
     $settings.locations.explorer = $Location
     Save-UserSettings $settings $env:USERNAME
+    Update-VbsWithNewLocation $Location
 }
 
 function Update-VbsWithNewLocation {
@@ -84,7 +79,7 @@ Else
 End If
 "@
 
-    Set-Content -Path $vbsPath -Value $content -Encoding ASCII
+    Set-Content -Path $VbsPath -Value $content -Encoding ASCII
 }
 
 function Initialize-Explorer {
@@ -98,32 +93,51 @@ function Initialize-Explorer {
 
     Update-VbsWithNewLocation $location
 
-    # Add or update registry entry
-    $regKey = "HKCU:\SOFTWARE\Classes\CLSID\{52205fd8-5dfb-447d-801a-d0b52f2e83e1}\shell\opennewwindow\command"
+    # # Add or update registry entry
+    # $regKey = "HKCU:\SOFTWARE\Classes\CLSID\{52205fd8-5dfb-447d-801a-d0b52f2e83e1}\shell\opennewwindow\command"
 
-    if (-not (Test-Path $regKey)) {
-        New-Item -Path $regKey -Force | Out-Null
+    # if (-not (Test-Path $regKey)) {
+    #     New-Item -Path $regKey -Force | Out-Null
+    # }
+
+    # Set-ItemProperty -Path $regKey -Name '(default)' -Value "wscript.exe `"$vbsPath`""
+    # Set-ItemProperty -Path $regKey -Name 'DelegateExecute' -Value ""
+    # Write-Host "Explorer launch location initialized."
+
+    # Add or update registry entry
+    if (-not (Test-Path $ExplorerCommandRegistryKey)) {
+        New-Item -Path $ExplorerCommandRegistryKey -Force | Out-Null
     }
 
-    Set-ItemProperty -Path $regKey -Name '(default)' -Value "wscript.exe `"$vbsPath`""
-    Set-ItemProperty -Path $regKey -Name 'DelegateExecute' -Value ""
+    Set-ItemProperty -Path $ExplorerCommandRegistryKey -Name '(default)' -Value "wscript.exe `"$vbsPath`""
+    Set-ItemProperty -Path $ExplorerCommandRegistryKey -Name 'DelegateExecute' -Value ""
+
     Write-Host "Explorer launch location initialized."
 }
 
 function Reset-ExplorerLocation {
+    # # Remove registry entry
+    # $regKey = "HKCU:\SOFTWARE\Classes\CLSID\{52205fd8-5dfb-447d-801a-d0b52f2e83e1}"
+    # if (Test-Path $regKey) {
+    #     Remove-Item -Path $regKey -Recurse -Force
+    #     Write-Host "Registry entry removed."
+    # }
+    # Write-Host "Explorer location reset."
+
     # Remove registry entry
-    $regKey = "HKCU:\SOFTWARE\Classes\CLSID\{52205fd8-5dfb-447d-801a-d0b52f2e83e1}"
-    if (Test-Path $regKey) {
-        Remove-Item -Path $regKey -Recurse -Force
+
+    if (Test-Path $ExplorerRegistryKey) {
+        Remove-Item -Path $ExplorerRegistryKey -Recurse -Force
         Write-Host "Registry entry removed."
     }
+
     Write-Host "Explorer location reset."
 }
 
 function Open-UserSetupJson {
     Write-Host
-    Write-Host "Bops.Lib.Setup.json is located at" -ForegroundColor Cyan -NoNewline; Write-Host ": " -ForegroundColor Magenta -NoNewline; Write-Host -ForegroundColor Yellow $jsonPath
-    code $jsonPath
+    Write-Host "Bops.Lib.Setup.json is located at" -ForegroundColor Cyan -NoNewline; Write-Host ": " -ForegroundColor Magenta -NoNewline; Write-Host -ForegroundColor Yellow $SettingsJsonPath
+    code $SettingsJsonPath
 }
 
 Export-ModuleMember -Function *
